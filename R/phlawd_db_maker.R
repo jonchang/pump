be8403130884561290f0f6dd9df09d3e79983c03
Jonathan Chau <jonathanqchau@gmail.com>
2021-11-18 08:38:25 -0800

    rename

--- R/phlawd_db_maker.R
+++ R/phlawd_db_maker.R
@@ -1,30 +1,34 @@
-#!/usr/bin/env Rscript
+# install.packages("rentrez")
+# install.packages("glue")
 
 library(tidyverse)
-library(RSQLite)
 library(rentrez)
+library(glue)
 library(XML)
 library(dplyr)
+library(RSQLite)
 
-# get the labridae database
-r_search <- entrez_search(db = "nuccore", term = "labridae[ORGN] AND cytb[GENE]")
-r_search <- entrez_search(db = "nucleotide", term = "labridae[Organism] AND cytb[Gene]", retmax = r_search$count)
-
-# prepare for data fetching
-RECORDS_PER_ITERATION <- 200
-iterations <- (r_search$count / RECORDS_PER_ITERATION) + 1
-
-# Initialize MySQL database
-mydb <- dbConnect(RSQLite::SQLite(), "")
-
-# as each batch is processed, add them into the sql database
-# fetch the data and wrangle the XML database
-for (i in 1:iterations) {
-  print(i)
-  upload <- entrez_post(db = "nucleotide", r_search$ids[RECORDS_PER_ITERATION*(i-1)+1:RECORDS_PER_ITERATION*i])
-  entrez_XML <- entrez_fetch(db = "nucleotide", rettype = "xml", retmode = "xml", 
-                             web_history = upload, retmax = RECORDS_PER_ITERATION, parsed = TRUE)
+mod_cbind <- function(df1, df2, colname, new_name) {
+  i <- 1
+  j <- 1
   
+  df1[new_name] <- NA
+  col <- df1[colname][[1]]
+  col2 <- df1[new_name][[1]]
+  
+  while (i <= length(col)) {
+    if (!is.na(col[i])) {
+      col2[i] <- df2[j]
+      j <- j + 1
+    }
+    i <- i + 1
+  }
+  
+  df1[new_name] <- col2
+  return(df1)
+}
+
+sqlAdd <- function(entrez_XML) {
   # extract the id
   print('extracting id...')
   id_df <- xmlToDataFrame(entrez_XML, nodes = getNodeSet(entrez_XML, "//GBSeqid"))
@@ -45,7 +49,8 @@ for (i in 1:iterations) {
   
   # combine three dataframes at once
   print('combining...')
-  labridae_df <- cbind(id_df, cbind(content_df, title_df))
+  mid_df <- mod_cbind(content_df, title_df, "GBSeq_references", "title")
+  labridae_df <- cbind(id_df, mid_df)
   
   # filter database after including everything
   print('filtering...')
@@ -55,7 +60,7 @@ for (i in 1:iterations) {
   batch_filtered <- labridae_df[, c('id', 'ncbi_id',
                                     'GBSeq_locus', 'GBSeq_primary-accession', 
                                     'GBSeq_accession-version', 'GBSeq_definition',
-                                    'title_df', 'GBSeq_sequence')]
+                                    'title', 'GBSeq_sequence')]
   
   names(batch_filtered)[3] <- 'locus'
   names(batch_filtered)[4] <- 'accession_id'
@@ -69,5 +74,53 @@ for (i in 1:iterations) {
   dbWriteTable(mydb, "sequences", batch_filtered, append = TRUE)
 }
 
-data_sql <- dbGetQuery(mydb, 'SELECT * FROM sequences')
+gene_names <- c("12s", "16s", "4c4", "coi", "cytb", "enc1", "ficd", "glyt", "hoxc6a", "kiaa1239", "myh6", "panx2", "plagl2", "ptr", "rag1", "rag2", "rhodopsin", "ripk4", "sh3px3", "sidkey", "sreb2", "svep1", "tbr1", "vcpip", "zic1")
+r_search1 <- list()
+r_search2 <- list()
+search_term_frame = "labridae[Organism] AND "
 
+for(i in 1:length(gene_names))
+{
+  r_glue <-  glue("{search_term_frame}{gene_names[i]}[Gene]")
+  r_glue2 <-  glue("{search_term_frame}{gene_names[i]}")
+  print(r_glue)
+  r_search1[[i]] <- entrez_search(db="nucleotide", term = r_glue)
+  r_search2[[i]] <- entrez_search(db="nucleotide", term = r_glue, retmax = r_search1[[i]]$count)
+  r_search1
+  if(r_search1[[i]]$count == 0)
+  {
+    print("search had 0 results, researching without gene field")
+    r_search1[[i]] <- entrez_search(db="nucleotide", term = r_glue2)
+    r_search2[[i]] <- entrez_search(db="nucleotide", term = r_glue2, retmax = r_search1[[i]]$count)
+    print(r_search2[[i]]$count)
+  }
+  else
+  {
+    print(r_search2[[i]]$count)
+  }
+}
+
+RECORDS_PER_ITERATION <- 200
+
+# Initialize SQL database
+mydb <- dbConnect(RSQLite::SQLite(), "")
+
+# real test is 1:length(r_search2)
+for(j in 1:length(r_search2)){
+  print(paste("fetching ", gene_names[j]))
+  iterations = (r_search2[[j]]$count / RECORDS_PER_ITERATION) + 1
+  
+  # real data is 1:iterations
+  for (i in 1:iterations) {
+    print(i)
+    upload <- entrez_post(db="nucleotide", r_search2[[j]]$ids[RECORDS_PER_ITERATION*(i-1)+1:RECORDS_PER_ITERATION*i])
+    curr_record <- entrez_fetch(db="nucleotide", rettype="xml", retmode = "xml", web_history=upload,
+                               retmax=RECORDS_PER_ITERATION, parsed = TRUE)
+    sqlAdd(curr_record)
+    
+    # TODO: catch any invalid records
+    # grab those records and store it into a logfile
+  }
+}
+
+data_sql <- dbGetQuery(mydb, 'SELECT * FROM sequences')
